88e5fd278cbb22f93481445fe8f47d55a23601e5
Imran <imranmajeed@outlook.com>
2024-07-03 16:19:19 +0500

    Update wplogin-mu-plugin.php

--- wplogin-mu-plugin.php
+++ wplogin-mu-plugin.php
@@ -1,29 +1,36 @@
 <?php
 /*
-Plugin Name: WPLogin mu-plugin
-Version: 1.0
+Plugin Name: WPLogin muplugin
+Version: 1.1
 Description: Functionality to my WordPress site.
 */
 
 // Disable Gutenberg
 add_filter( 'use_block_editor_for_post', '__return_false' );
 
-
 // wp-admin logo URL and title
 add_action('login_headerurl', function () { return home_url(); });
-add_action('login_headertitle', function () { return wp_title(); });
+add_action('login_headertext', function () { return wp_title('', false); });
 
 // wp-admin_logo image and style
 add_action('login_enqueue_scripts', function () {
 	$logo_id = get_theme_mod('custom_logo');
-	$logo_url = wp_get_attachment_image_url($logo_id, 'full');
-	echo "<style>#login h1 a {background-image: url('".esc_url($logo_url)."'); }</style>";
+	if ($logo_id) {
+		$logo_url = wp_get_attachment_image_url($logo_id, 'full');
+		echo "<style id='logo_url'>#login h1 a {background-image: url('".esc_url($logo_url)."'); }</style>";
+	}
 	?>
 	<style type="text/css">
 		#login h1 a {min-height: 130px; width: 100%; background-size: contain; background-position: center center; }
 		body.login{background:#fcfcff;}
+		#login form p.submit input#wp-submit {background: #2271b1; font-size: 20px; margin-top: 10px; padding: 0px 30px; }
+		#login form p.submit {display: flex; width: 100%; text-align: center; justify-content: center; }
+		.login form {padding-bottom: 15px; }
 	</style>
 	<?php 
+	// Get the custom CSS for the admin area
+	$custom_css = wp_get_custom_css();
+	if (!empty($custom_css)) {echo '<style id="custom_css" type="text/css">' . wp_strip_all_tags($custom_css) . '</style>'; }
 });
 
 //wp-admin header
