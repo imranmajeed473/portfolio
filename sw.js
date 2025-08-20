d7d5e2b3a895792445e0c01007a19b9efdcae443
Imran <imranmajeed@outlook.com>
2025-08-20 14:17:54 +0500

    Create sw.js Web App
    Web App
    Progressive Web App (PWA)

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "portfolio-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/scripts.js",
+  "/assets/img/profile.jpg",
+  "/assets/img/favicon.ico"
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch from cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate SW and clean old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
